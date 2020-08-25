959f6eead588057aca33ff1e1ebb7257757d0e2c
Vinicius Ferrari <viniciusferrari@cvccorp.com.br>
2020-08-25 13:09:55 -0300

    Fixed some warnings

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,17 @@ import PokemonSearch from './presenter/PokemonSearch';
 export default class App extends Component {
 
   render() {
+    const pokemonsSearched = require('../src/pokemon.json')
+      .map((pokemonName: string) => pokemonName.toLocaleLowerCase());
+
     return (
       <div className="App">
-        <PokemonSearch name="bulbasaur" />
-        <PokemonSearch name="ivysaur" />
-        <PokemonSearch name="venusaur" />
-        <PokemonSearch name="charmander" />
-        <PokemonSearch name="charmeleon" />
-        <PokemonSearch name="charizard" />
-        <PokemonSearch name="squirtle" />
-        <PokemonSearch name="wartortle" />
-        <PokemonSearch name="blastoise" />
-        <PokemonSearch name="caterpie" />
-        <PokemonSearch name="metapod" />
-        <PokemonSearch name="butterfree" />
-        <PokemonSearch name="weedle" />
-        <PokemonSearch name="kakuna" />
-        <PokemonSearch name="beedrill" />
-        <PokemonSearch name="pidgey" />
-        <PokemonSearch name="pidgeotto" />
-        <PokemonSearch name="pidgeot" />
-        <PokemonSearch name="rattata" />
-        <PokemonSearch name="raticate" />
-        <PokemonSearch name="spearow" />
-        <PokemonSearch name="fearow" />
-        <PokemonSearch name="ekans" />
-        <PokemonSearch name="arbok" />
-        <PokemonSearch name="pikachu" />
-        <PokemonSearch name="raichu" />  
-        <PokemonSearch name="growlithe" />  
+        {
+          pokemonsSearched.map(
+            (value: string, index: number) => {
+              return <PokemonSearch key={index} name={value} />
+            })
+        }
       </div>
     );
   }
